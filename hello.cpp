81bc589730e6c00c456ec502f686a9254fc90097
Xudehua <dehua.xu@nbbsw.com>
2017-07-12 13:24:49 +0800

    v1.3

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 
-
+/**
+ * 定义一个模板函数
+ */
 template <typename T>
 T Max(T arg1, T arg2)
 {
 	return arg1 > arg2 ? arg1 : arg2;
 }
+
+/**
+ * 定义一个模板类
+ */
+template <typename T>
+class Class
+{
+public:
+	Class(T arg);
+	void display(void);
+};
 
 int main(int argc, char *argv[])
 {
